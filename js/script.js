939fb75ca9ca7e11be7e125a171563a3ffb267b8
Moges Sisay <mogesse741@gmail.com>
2025-10-07 11:10:06 +0300

    Add slideToggle to show/hide footer links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(".footer-links-wrapper h3").click(function() {
-  if ($(window).width() <= 768) {}
-  
+  if ($(window).width() <= 768) {
+    $(this).next().slideToggle();
+    $(this).toggleClass("iconRotator");
+  }
+
 });
